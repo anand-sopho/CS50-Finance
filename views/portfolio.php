0252e80ec73f9de76717dc3d35134b0ea4d7cd84
Anand Chauhan <chauhan.anand@iitgn.ac.in>
2016-12-11 15:46:52 +0530

    Portfolio v.01

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,3 +1,28 @@
-<div>
-    <iframe allowfullscreen frameborder="0" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3&rel=0" width="420"></iframe>
+<div id="message">
+		<h3> Your current balance is
+		<b><?=sprintf("%.2f",$cash[0]["cash"]) ?></b> $.  </h3>
+		<br/>	
 </div>
+	<table class="table table-striped">
+	<thead>
+        <tr>
+	        <th>Symbol</th>
+	        <th>Name</th>
+	        <th>Shares</th>
+	        <th>Price</th>
+        </tr>
+    </thead>
+    
+    <tbody>
+
+		<?php foreach ($positions as $position): ?>
+		
+			<tr>    
+        	<td><?= $position["symbol"] ?></td>
+        	<td><?= $position["name"] ?></td>
+        	<td><?= $position["shares"] ?></td>
+        	<td><?= "$" . $position["price"] ?></td>
+   			</tr>
+   			<?php endforeach ?>
+	</tbody>
+</table>
